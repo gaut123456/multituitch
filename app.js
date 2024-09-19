df55309b83316c1ba1f1148ed80f24e8a61a9c30
gauthier le rentier <gauthier.duffier@efrei.net>
2024-09-19 15:29:41 +0200

    first commit

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import connectDB from './config/db.js';
 import callbackRoutes from './routes/callbackRoute.js';
 import getFollowedRoutes from './routes/getFollowedRoute.js';
 import addFavoritesRoutes from './routes/addFavoritesRoute.js';
+import getFollowedLiveRoutes from './routes/getFollowedLiveRoute.js';
 import cors from 'cors';
 
 const app = express();
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use('/callback', callbackRoutes);
 app.use('/getFollowed', getFollowedRoutes);
 app.use('/addFavorites', addFavoritesRoutes);
+app.use('/getFollowedLive', getFollowedLiveRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
